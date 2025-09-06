0b32b508d12b0d4e5bc147c9a27a3d0ac40729c3
Mostafizur Rahman <94890704+Mostafizur979@users.noreply.github.com>
2025-09-06 21:04:06 +0600

    Add product descriptions and assets to getProducts
    Extended the getProducts function to include product descriptions and assets by adding getProductDescription and getProductAssets helper functions. Each product now includes 'descriptions' and 'assets' fields populated from related database tables.

--- api/products/getProducts.py
+++ api/products/getProducts.py
@@ -13,9 +13,46 @@ def getProducts(cursor):
                product['Image'] = base64.b64encode(product['Image']).decode('utf-8')
             except:
                 product['Image'] = "no-image"  
-
+                
+            product['descriptions'] = getProductDescription(product['SKU'],cursor)
+            product['assets'] = getProductAssets(product['SKU'],cursor)
             product_list.append(product)
         return  product_list
 
     except Exception as e:
         return e
+    
+def getProductDescription(sku,cursor):
+    q = "select * from product_description where productId='{}'".format(sku)    
+    cursor.execute(q)
+    result=cursor.fetchall()
+            
+    descriptionList = []
+    for data in result:
+        description = {
+            'id' : data[1],
+            'title': data[2],
+            'description': data[3]
+        }
+        descriptionList.append(description)
+    
+    return descriptionList
+
+def getProductAssets(sku,cursor):
+    q= "select * from product_asset where productId = '{}'".format(sku)
+    cursor.execute(q)
+    result=cursor.fetchall()
+
+    assetList = []
+    for data in result:
+        try:
+            image = base64.b64encode(data[2]).decode('utf-8')
+        except:
+            image = "no-image" 
+        asset = {
+            'id': data[1],
+            'image_url': image
+        }
+        assetList.append(asset)
+    return assetList
+    
